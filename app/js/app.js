c3842dd12bd23980d83efa811d8aa9fb1df51c25
unknown <kazakovayana@outlook.com>
2020-09-27 00:38:27 +0300

    driver section

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // this function runs when the DOM is ready, i.e. when the document has been parsed
-
-  // all js code should go below this line
-
 
   // burger menu logic
 document.querySelector('.header__burger').onclick = function() {
@@ -11,4 +7,67 @@ document.querySelector('.header__burger').onclick = function() {
   document.querySelector('.header__menu-background-mobile').classList.toggle('active');
   document.querySelector('body').classList.toggle('lock');
 }
+
+  // drivers section
+  fetch('/data.json')
+    .then(res => res.json())
+    .then(renderDrivers);
 });
+
+const renderDrivers = ({ drivers }) => {
+  const container = document.getElementById('drivers-container')
+
+  drivers.map((driver, i) => {
+    const date = new Date(driver.dateOfBirth)
+    const dateString = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+
+    const reverse = (i % 2) !== 0
+
+    container.insertAdjacentHTML('beforeend',`
+        <div class="driver-container-mobile">
+            <img src="${driver.imageUrl}" class="driver-photo" />
+            <div class="driver-position">${driver.position}</div>
+            <div class="line"></div>
+            <div class="driver-name">${driver.firstName} ${driver.lastName}</div>
+            <div class="description">
+                <div class="column">
+                    <div>Races: <span>${driver.races}</span></div>
+                    <div>Pole-positions: <span>${driver.polePositions}</span></div>
+                    <div>Podiums: <span>${driver.podiums}</span></div>
+                </div>
+                <div class="column--last">
+                    <div>Wins: <span>${driver.wins}</span></div>
+                    <div>Date of birth: <span>${dateString}</span></div>
+                    <div>Place of birth: <span>${driver.city}, ${driver.country}</span></div>
+                </div>
+            </div>
+        </div>
+      `);
+    container.insertAdjacentHTML('beforeend',`
+          <div class="driver-container${reverse ? '-reverse' : ''}">
+            <div class="line"></div>
+            <div class="driver-profile">
+              <img src="${driver.imageUrl}" class="driver-photo" />
+              <div class="driver-info">
+                  <div class="title">
+                      <div class="driver-position">${driver.position}</div>
+                      <div class="driver-name">${driver.firstName} ${driver.lastName}</div>
+                  </div>
+                  <div class="description">
+                    <div class="column">
+                      <div>Races: <span>${driver.races}</span></div>
+                      <div>Pole-positions: <span>${driver.polePositions}</span></div>
+                      <div>Podiums: <span>${driver.podiums}</span></div>
+                    </div>
+                    <div class="column--last">
+                      <div>Wins: <span>${driver.wins}</span></div>
+                      <div>Date of birth: <span>${dateString}</span></div>
+                      <div>Place of birth: <span>${driver.city}, ${driver.country}</span></div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        `);
+  })
+}
